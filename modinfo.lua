faa9c7bbccc05a8ac79904a4921c1a426440ddbc
Tony Kalou <cr4shmaster@hotmail.com>
2016-02-25 21:53:22 +0200

    major update
    more items are now stackable; added custom stack size for
    small/medium/large; added options for removing murder; added options for
    removing feeding;

--- modinfo.lua
+++ modinfo.lua
@@ -1,41 +1,162 @@
-name = "Stacks"
-description = "Allows everything to stack up to your desired value."
+name = "Stacks v2.0"
+description = "Modifies the maximum stack size of all stackable items."
 author = "cr4shmaster"
-version = "1.3.2"
-
+version = "2.0.0"
 forumthread = ""
-
 api_version = 10
-
 all_clients_require_mod = true
 dst_compatible = true
 client_only_mod = false
-
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 
-configuration_options = {
-
-	{
-		name = "StacksConfig",
-		label = "Maximum stack size:",
-		options = {
-			{description = "10", data = 10},
-			{description = "20", data = 20},
-			{description = "40", data = 40},
-			{description = "50", data = 50},
-			{description = "60", data = 60},
-			{description = "70", data = 70},
-			{description = "80", data = 80},
-			{description = "90", data = 90},
-			{description = "100", data = 100},
-			{description = "200", data = 200},
-			{description = "500", data = 500},
-			{description = "1000", data = 1000},
-			{description = "9999", data = 9999},
-		},
-		default = 100,
-	},
+local crsToggle = {
+ {description = "Enabled", data = 1},
+ {description = "Disabled", data = 0},
 }
 
-server_filter_tags = {"Stacks"}
+local crsStacks = {
+ {description = "5", data = 5},
+ {description = "10", data = 10},
+ {description = "15", data = 15},
+ {description = "20", data = 20},
+ {description = "25", data = 25},
+ {description = "30", data = 30},
+ {description = "35", data = 35},
+ {description = "40", data = 40},
+ {description = "45", data = 45},
+ {description = "50", data = 50},
+ {description = "55", data = 55},
+ {description = "60", data = 60},
+ {description = "65", data = 65},
+ {description = "70", data = 70},
+ {description = "75", data = 75},
+ {description = "80", data = 80},
+ {description = "85", data = 85},
+ {description = "90", data = 90},
+ {description = "95", data = 95},
+ {description = "100", data = 100},
+ {description = "150", data = 150},
+ {description = "200", data = 200},
+ {description = "250", data = 250},
+ {description = "500", data = 500},
+ {description = "1000", data = 1000},
+ {description = "9999", data = 9999},
+}
+
+configuration_options = {
+ {
+  name = "crsChangeSmallStacksSize",
+  label = "Small Stacks",
+  options = crsStacks,
+  default = 100,
+ },
+ {
+  name = "crsChangeMediumStacksSize",
+  label = "Medium Stacks",
+  options = crsStacks,
+  default = 100,
+ },
+ {
+  name = "crsChangeLargeStacksSize",
+  label = "Large Stacks",
+  options = crsStacks,
+  default = 100,
+ },
+  -- bees
+ {
+  name = "crsBeesDontDieToggle",
+  label = "Bees Never Die",
+  options = crsToggle,
+  default = 1,
+ },
+ {
+  name = "crsBeesRemoveMurderToggle",
+  label = "Can't Murder Bees",
+  options = crsToggle,
+  default = 1,
+ },
+ -- butterflies
+ {
+  name = "crsButterfliesDontDieToggle",
+  label = "Butterflies Never Die",
+  options = crsToggle,
+  default = 1,
+ },
+ {
+  name = "crsButterfliesRemoveMurderToggle",
+  label = "Can't Murder Butterflies",
+  options = crsToggle,
+  default = 1,
+ },
+ -- rabbits
+ {
+  name = "crsRabbitsDontDieToggle",
+  label = "Rabbits Never Die",
+  options = crsToggle,
+  default = 1,
+ },
+ {
+  name = "crsRabbitsCanStackToggle",
+  label = "Stackable Rabbits",
+  options = crsToggle,
+  default = 1,
+ },
+ {
+  name = "crsRabbitsRemoveMurderToggle",
+  label = "Can't Murder Rabbits",
+  options = crsToggle,
+  default = 1,
+ },
+ -- birds
+ {
+  name = "crsBirdsDontDieToggle",
+  label = "Birds Never Die",
+  options = crsToggle,
+  default = 1,
+ },
+ {
+  name = "crsBirdsCanStackToggle",
+  label = "Stackable Birds",
+  options = crsToggle,
+  default = 1,
+ },
+ {
+  name = "crsBirdsRemoveMurderToggle",
+  label = "Can't Murder Birds",
+  options = crsToggle,
+  default = 1,
+ },
+ -- moles
+ {
+  name = "crsMolesDontDieToggle",
+  label = "Moles Never Die",
+  options = crsToggle,
+  default = 1,
+ },
+ {
+  name = "crsMolesCanStackToggle",
+  label = "Stackable Moles",
+  options = crsToggle,
+  default = 1,
+ },
+ {
+  name = "crsMolesRemoveMurderToggle",
+  label = "Can't Murder Moles",
+  options = crsToggle,
+  default = 1,
+ },
+ -- mosquitos
+ {
+  name = "crsMosquitosDontDieToggle",
+  label = "Mosquitos Never Die",
+  options = crsToggle,
+  default = 1,
+ },
+ {
+  name = "crsMosquitosRemoveMurderToggle",
+  label = "Can't Murder Mosquitos",
+  options = crsToggle,
+  default = 1,
+ },
+}
--- modmain.lua
+++ modmain.lua
@@ -1,22 +1,22 @@
 local require = GLOBAL.require
 local stackable_replica = require "components/stackable_replica"
 
-GLOBAL.TUNING.STACK_SIZE_LARGEITEM = GetModConfigData("StacksConfig")
-GLOBAL.TUNING.STACK_SIZE_MEDITEM = GetModConfigData("StacksConfig")
-GLOBAL.TUNING.STACK_SIZE_SMALLITEM = GetModConfigData("StacksConfig") 
+TUNING.STACK_SIZE_LARGEITEM = GetModConfigData("crsChangeLargeStacksSize");
+TUNING.STACK_SIZE_MEDITEM = GetModConfigData("crsChangeMediumStacksSize");
+TUNING.STACK_SIZE_SMALLITEM = GetModConfigData("crsChangeSmallStacksSize"); 
 
 local stackable_replica_ctorBase = stackable_replica._ctor or function() return true end    
 
 function stackable_replica._ctor(self, inst)
-    self.inst = inst
-    self._stacksize = GLOBAL.net_ushortint(inst.GUID, "stackable._stacksize", "stacksizedirty")
-    self._maxsize = GetModConfigData("StacksConfig")
+ self.inst = inst
+ self._stacksize = GLOBAL.net_ushortint(inst.GUID, "stackable._stacksize", "stacksizedirty")
+ self._maxsize = GetModConfigData("crsChangeSmallStacksSize")
 end
 
 local stackable_replicaSetMaxSize_Base = stackable_replica.SetMaxSize or function() return true end
 
 function stackable_replica:SetMaxSize(maxsize)
-	self._maxsize = GetModConfigData("StacksConfig")
+	self._maxsize = GetModConfigData("crsChangeSmallStacksSize")
 end
 
 local stackable_replicaMaxSize_Base = stackable_replica.MaxSize or function() return true end
@@ -24,3 +24,89 @@ local stackable_replicaMaxSize_Base = stackable_replica.MaxSize or function() re
 function stackable_replica:MaxSize()
 	return self._maxsize
 end
+
+local function crsMakeStackable(inst)
+ inst:AddComponent("stackable")
+ inst.components.stackable.maxsize = TUNING.STACK_SIZE_SMALLITEM
+end
+
+local function crsUnmakeFeedable(inst)
+ inst:RemoveComponent("perishable")
+ inst.components.inventoryitem:SetOnPutInInventoryFn(function(inst)
+  if oninventory then
+   oninventory(inst)
+  end
+  end)
+ inst.components.inventoryitem:SetOnDroppedFn(function(inst)
+  if ondropped then
+   ondropped(inst)
+  end
+  end)
+end
+
+local function crsRemoveMurder(inst)
+ if inst.components.health then
+  inst.components.health.canmurder = false
+ end
+end
+ 
+ -- make stackable
+ AddPrefabPostInit("minotaurhorn", crsMakeStackable)
+ AddPrefabPostInit("tallbirdegg", crsMakeStackable)
+ if GetModConfigData("crsRabbitsCanStackToggle") then
+  AddPrefabPostInit("rabbit", crsMakeStackable)
+ end
+ if GetModConfigData("crsBirdsCanStackToggle")then
+  AddPrefabPostInit("crow", crsMakeStackable)
+  AddPrefabPostInit("robin", crsMakeStackable)
+  AddPrefabPostInit("robin_winter", crsMakeStackable)
+ end
+ if GetModConfigData("crsMolesCanStackToggle") then
+  AddPrefabPostInit("mole", crsMakeStackable)
+ end
+ 
+ -- remove feedable
+ if GetModConfigData("crsMolesDontDieToggle") then
+  AddPrefabPostInit("mole", crsUnmakeFeedable)
+ end
+ if GetModConfigData("crsRabbitsDontDieToggle") then
+  AddPrefabPostInit("rabbit", crsUnmakeFeedable)
+ end
+ if GetModConfigData("crsBirdsDontDieToggle") then
+  AddPrefabPostInit("crow", crsUnmakeFeedable)
+  AddPrefabPostInit("robin", crsUnmakeFeedable)
+  AddPrefabPostInit("robin_winter", crsUnmakeFeedable)
+ end
+ if GetModConfigData("crsBeesDontDieToggle") then
+  AddPrefabPostInit("bee", crsUnmakeFeedable)
+  AddPrefabPostInit("killerbee", crsUnmakeFeedable)
+ end
+ if GetModConfigData("crsButterfliesDontDieToggle") then
+  AddPrefabPostInit("butterfly", crsUnmakeFeedable)
+ end
+ if GetModConfigData("crsMosquitosDontDieToggle") then
+  AddPrefabPostInit("mosquito", crsUnmakeFeedable)
+ end
+
+ -- remove murder
+ if GetModConfigData("crsMolesRemoveMurderToggle") then
+  AddPrefabPostInit("mole", crsRemoveMurder)
+ end
+ if GetModConfigData("crsBeesRemoveMurderToggle") then
+  AddPrefabPostInit("bee", crsRemoveMurder)
+  AddPrefabPostInit("killerbee", crsRemoveMurder)
+ end
+ if GetModConfigData("crsButterfliesRemoveMurderToggle") then
+  AddPrefabPostInit("butterfly", crsRemoveMurder)
+ end
+ if GetModConfigData("crsRabbitsRemoveMurderToggle") then
+  AddPrefabPostInit("rabbit", crsRemoveMurder)
+ end
+ if GetModConfigData("crsBirdsRemoveMurderToggle") then
+  AddPrefabPostInit("crow", crsRemoveMurder)
+  AddPrefabPostInit("robin", crsRemoveMurder)
+  AddPrefabPostInit("robin_winter", crsRemoveMurder)
+ end
+ if GetModConfigData("crsMosquitosRemoveMurderToggle") then
+  AddPrefabPostInit("mosquito", crsRemoveMurder)
+ end
